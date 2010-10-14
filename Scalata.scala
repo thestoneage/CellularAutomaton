f9ff66549eab63817336d19a53f643a436ad8993
Marc Rummel <marc.rummmel@gmail.com>
2010-10-14 17:45:38 +0200

    Function from forms a evolutionary stream out of a CA

--- Scalata.scala
+++ Scalata.scala
@@ -1,23 +1,24 @@
+import scala.collection.immutable.Stream
 
 class ElementaryCellularAutomata(rule:Int, initial:List[Boolean]) {
-    assert(number < 256)
+    assert(rule < 256)
 
     def rule(slice:List[Boolean]):Boolean = {
         slice match {
-            case List(false, false, false) => (number & 0x01) == 0x01
-            case List(false, false, true)  => (number & 0x02) == 0x02
-            case List(false, true,  false) => (number & 0x04) == 0x04
-            case List(false, true,  true)  => (number & 0x08) == 0x08
-            case List(true,  false, false) => (number & 0x10) == 0x10
-            case List(true,  false, true)  => (number & 0x20) == 0x20
-            case List(true,  true,  false) => (number & 0x40) == 0x40
-            case List(true,  true,  true)  => (number & 0x80) == 0x80
+            case List(false, false, false) => (rule & 0x01) == 0x01
+            case List(false, false, true)  => (rule & 0x02) == 0x02
+            case List(false, true,  false) => (rule & 0x04) == 0x04
+            case List(false, true,  true)  => (rule & 0x08) == 0x08
+            case List(true,  false, false) => (rule & 0x10) == 0x10
+            case List(true,  false, true)  => (rule & 0x20) == 0x20
+            case List(true,  true,  false) => (rule & 0x40) == 0x40
+            case List(true,  true,  true)  => (rule & 0x80) == 0x80
         }
     }
 
-    def applyRule() = {
+    def next = {
         val lst = ((initial.last :: initial) :+ initial.head).sliding(3).map(x => rule(x)).toList
-        new ElementaryCellularAutomata(lst, number)
+        new ElementaryCellularAutomata(rule, lst)
     }
 
     override def toString() = {
@@ -26,10 +27,9 @@ class ElementaryCellularAutomata(rule:Int, initial:List[Boolean]) {
 
 }
 
-def printAutomata(automata:ElementaryCellularAutomata, times:Int):Unit = {
-    println(automata)
-    if (times > 1)
-        printAutomata(automata.applyRule(), times - 1)
+def from(automaton:ElementaryCellularAutomata):Stream[ElementaryCellularAutomata] = {
+    Stream.cons(automaton, from(automaton.next))
 }
+
 
 // vim: set ts=4 sw=4 et:
