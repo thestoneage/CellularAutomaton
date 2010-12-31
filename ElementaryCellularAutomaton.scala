8bb35fc85a3bdc89c6ae904df12472bf59c6049c
Jörg W Mittag <JoergWMittag+Scala@GoogleMail.Com>
2010-12-31 15:13:45 +0100

    Unofficial Scala coding style: 2-space indentation.
    See: <http://Davetron5000.GitHub.Com/scala-style/indentation/>.

--- ElementaryCellularAutomaton.scala
+++ ElementaryCellularAutomaton.scala
@@ -1,38 +1,38 @@
 import scala.collection.immutable.Stream
 
 object ElementaryCellularAutomaton {
-    def apply(rule:Int, initial:List[Boolean]) = new ElementaryCellularAutomaton(rule, initial)
+  def apply(rule:Int, initial:List[Boolean]) = new ElementaryCellularAutomaton(rule, initial)
 
-    def evolveFrom(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
-        automaton #:: evolveFrom(automaton.next)
+  def evolveFrom(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
+    automaton #:: evolveFrom(automaton.next)
 }
 
 class ElementaryCellularAutomaton(rule:Int, initial:List[Boolean]) {
-    assert(rule < 256)
+  assert(rule < 256)
 
-    def rule(slice:List[Boolean]):Boolean = slice match {
-        case List(false, false, false) => (rule & 0x01) == 0x01
-        case List(false, false, true ) => (rule & 0x02) == 0x02
-        case List(false, true,  false) => (rule & 0x04) == 0x04
-        case List(false, true,  true ) => (rule & 0x08) == 0x08
-        case List(true,  false, false) => (rule & 0x10) == 0x10
-        case List(true,  false, true ) => (rule & 0x20) == 0x20
-        case List(true,  true,  false) => (rule & 0x40) == 0x40
-        case List(true,  true,  true ) => (rule & 0x80) == 0x80
-    }
+  def rule(slice:List[Boolean]):Boolean = slice match {
+    case List(false, false, false) => (rule & 0x01) == 0x01
+    case List(false, false, true ) => (rule & 0x02) == 0x02
+    case List(false, true,  false) => (rule & 0x04) == 0x04
+    case List(false, true,  true ) => (rule & 0x08) == 0x08
+    case List(true,  false, false) => (rule & 0x10) == 0x10
+    case List(true,  false, true ) => (rule & 0x20) == 0x20
+    case List(true,  true,  false) => (rule & 0x40) == 0x40
+    case List(true,  true,  true ) => (rule & 0x80) == 0x80
+  }
 
-    def next = {
-        val lst = ((initial.last :: initial) :+ initial.head).sliding(3).map(rule _).toList
-        ElementaryCellularAutomaton(rule, lst)
-    }
+  def next = {
+    val lst = ((initial.last :: initial) :+ initial.head).sliding(3).map(rule _).toList
+    ElementaryCellularAutomaton(rule, lst)
+  }
 
-    def evolutionStream =
-        this #:: evolve(next)
+  def evolutionStream =
+    this #:: evolve(next)
 
-    override def toString() = initial.map(if (_) "+" else "-").mkString
+  override def toString() = initial.map(if (_) "+" else "-").mkString
 
-    private def evolve(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
-        this #:: evolve(next)
+  private def evolve(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
+    this #:: evolve(next)
 }
 
-// vim: set ts=4 sw=4 et:
+// vim: set ts=2 sw=2 et:
