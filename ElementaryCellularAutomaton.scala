6ea1629d34c634dd31cf0e9f771ee284df0f9a44
Jörg W Mittag <JoergWMittag+Scala@GoogleMail.Com>
2010-12-31 17:21:11 +0100

    Even more coding style.

--- ElementaryCellularAutomaton.scala
+++ ElementaryCellularAutomaton.scala
@@ -1,16 +1,16 @@
 import scala.collection.immutable.Stream
 
 object ElementaryCellularAutomaton {
-  def apply(rule:Short, initial:List[Boolean]) = new ElementaryCellularAutomaton(rule, initial)
+  def apply(rule: Short, initial: List[Boolean]) = new ElementaryCellularAutomaton(rule, initial)
 
-  def evolveFrom(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
+  def evolveFrom(automaton: ElementaryCellularAutomaton): Stream[ElementaryCellularAutomaton] =
     automaton #:: evolveFrom(automaton.next)
 }
 
-class ElementaryCellularAutomaton(rule:Short, initial:List[Boolean]) {
+class ElementaryCellularAutomaton(rule: Short, initial: List[Boolean]) {
   assert(rule < 256)
 
-  def rule(slice:List[Boolean]):Boolean = slice match {
+  def rule(slice: List[Boolean]): Boolean = slice match {
     case List(false, false, false) => (rule & 0x01) == 0x01
     case List(false, false, true ) => (rule & 0x02) == 0x02
     case List(false, true,  false) => (rule & 0x04) == 0x04
@@ -30,7 +30,7 @@ class ElementaryCellularAutomaton(rule:Short, initial:List[Boolean]) {
 
   override def toString() = initial.map(if (_) "+" else "-").mkString
 
-  private def evolve(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
+  private def evolve(automaton: ElementaryCellularAutomaton): Stream[ElementaryCellularAutomaton] =
     this #:: evolve(next)
 }
 
