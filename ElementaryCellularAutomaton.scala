da04acb87200eba7369d8553e1b4c4f8615e25e3
Jörg W Mittag <JoergWMittag+Scala@GoogleMail.Com>
2010-12-31 14:58:22 +0100

    Remove unnecessary curly braces.

--- ElementaryCellularAutomaton.scala
+++ ElementaryCellularAutomaton.scala
@@ -3,16 +3,15 @@ import scala.collection.immutable.Stream
 object ElementaryCellularAutomaton {
     def apply(rule:Int, initial:List[Boolean]) = new ElementaryCellularAutomaton(rule, initial)
 
-    def evolveFrom(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] = {
+    def evolveFrom(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
         automaton #:: evolveFrom(automaton.next)
-    }
 }
 
 class ElementaryCellularAutomaton(rule:Int, initial:List[Boolean]) {
 
     assert(rule < 256)
 
-    def rule(slice:List[Boolean]):Boolean = {
+    def rule(slice:List[Boolean]):Boolean =
         slice match {
             case List(false, false, false) => (rule & 0x01) == 0x01
             case List(false, false, true ) => (rule & 0x02) == 0x02
@@ -23,24 +22,20 @@ class ElementaryCellularAutomaton(rule:Int, initial:List[Boolean]) {
             case List(true,  true,  false) => (rule & 0x40) == 0x40
             case List(true,  true,  true ) => (rule & 0x80) == 0x80
         }
-    }
 
     def next:ElementaryCellularAutomaton = {
         val lst = ((initial.last :: initial) :+ initial.head).sliding(3).map(rule _).toList
         new ElementaryCellularAutomaton(rule, lst)
     }
 
-    def evolutionStream:Stream[ElementaryCellularAutomaton] = {
+    def evolutionStream:Stream[ElementaryCellularAutomaton] =
         this #:: evolve(this.next)
-    }
 
-    override def toString() = {
+    override def toString() =
         initial.map(if (_) "+" else "-").mkString
-    }
 
-    private def evolve(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] = {
+    private def evolve(automaton:ElementaryCellularAutomaton):Stream[ElementaryCellularAutomaton] =
         this #:: evolve(this.next)
-    }
 }
 
 // vim: set ts=4 sw=4 et:
